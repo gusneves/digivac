8718dfefe5111711f97b081f79d7bc94e31965a6
matheusranzani <ranzani.matheus@gmail.com>
2020-08-27 13:44:12 -0300

    Mudança no routes do app e botão de logout na home

--- src/screens/authenticaded/Home.js
+++ src/screens/authenticaded/Home.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import { SessionContext } from '../../context/Session';
 
 export default function Home() {
+  const { signOut } = useContext(SessionContext);
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <TouchableOpacity onPress={signOut}>
+        <Text style={{ color: '#00f' }}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
