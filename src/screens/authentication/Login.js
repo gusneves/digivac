431cf265ae9d0d2ded2d7586da6036998ccb98db
Gustavo Neves <gustavon@pop-os.localdomain>
2020-08-31 12:14:16 -0300

    Finished Profile and Login screens

--- src/screens/authentication/Login.js
+++ src/screens/authentication/Login.js
@@ -1,118 +1,187 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useRef, useContext, useState } from "react";
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
+import { Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
-import { SessionContext } from '../../context/Session';
+import { SessionContext } from "../../context/Session";
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [visibilityIcon, useVisibilityIcon] = useState("visibility-off");
+    const [secureEntry, useSecureEntry] = useState(true);
 
-  const { signIn } = useContext(SessionContext);
+    function changeVisibility(){
+        visibilityIcon === "visibility-off" ? useVisibilityIcon("visibility") : useVisibilityIcon("visibility-off")
+        useSecureEntry(!secureEntry)
+        console.log(visibilityIcon, secureEntry);
+    }
 
-  function handleSubmmit() {
-    signIn(email, password);
-  }
+    const formSchema = Yup.object().shape({
+        email: Yup.string()
+            .required("Campo obrigatório!")
+            .email("Por favor, insira um e-mail válido!"),
+        password: Yup.string().required("Campo obrigatório!"),
+    });
 
-  return (
-    <View style={styles.container}>
-      <Image style={styles.logo} source={logo}/>
+    const { signIn } = useContext(SessionContext);
 
-      <View style={styles.form}>
-        <Text style={styles.label}>SEU E-MAIL *</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='Seu e-mail'
-          placeholderTextColor='#999'
-          keyboardType='email-address'
-          autoCapitalize='none'
-          autoCorrect={false}
-          value={email}
-          onChangeText={setEmail}
-        />
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            enabled={Platform.OS === "ios"}
+            behavior="padding"
+        >
+            <Image style={styles.logo} source={logo} />
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                onSubmit={async (values, errors) => {
+                    await signIn(values.email, values.password).then(() => {
+                        errors.setFieldError(
+                            "password",
+                            "Usuário ou senha incorretos!"
+                        );
+                    });
+                }}
+                validationSchema={formSchema}
+            >
+                {({ values, handleChange, handleSubmit, errors }) => (
+                    <View style={styles.form}>
+                        <Input
+                            label="E-mail"
+                            value={values.email}
+                            onChangeText={handleChange("email")}
+                            leftIcon={() => (
+                                <Icon
+                                    name="mail"
+                                    color="#AAA"
+                                    size={20}
+                                    style={{ marginRight: 4 }}
+                                />
+                            )}
+                            labelStyle={styles.inputLabel}
+                            inputStyle={styles.input}
+                            placeholder="Seu e-mail"
+                            placeholderTextColor="#999"
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            errorMessage={errors.email}
+                        />
 
-        <Text style={styles.label}>SUA SENHA *</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='Sua senha'
-          placeholderTextColor='#999'
-          autoCapitalize='none'
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
+                        <Input
+                            label="Senha"
+                            value={values.password}
+                            onChangeText={handleChange("password")}
+                            leftIcon={() => (
+                                <Icon
+                                    name="lock"
+                                    color="#AAA"
+                                    size={20}
+                                    style={{ marginRight: 4 }}
+                                />
+                            )}
+                            rightIcon={() => (
+                                <Icon
+                                    name={visibilityIcon}
+                                    color="#AAA"
+                                    size={20}
+                                    style={{ marginRight: 4 }}
+                                    onPress={() => {changeVisibility()}}
+                                />
+                            )}
+                            labelStyle={styles.inputLabel}
+                            inputStyle={styles.input}
+                            placeholder="Sua senha"
+                            placeholderTextColor="#999"
+                            autoCapitalize="none"
+                            secureTextEntry={secureEntry}
+                            errorMessage={errors.password}
+                            errorStyle={styles.error}
+                        />
 
-        <TouchableOpacity onPress={handleSubmmit} style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => { navigation.navigate('Cadastro') }} style={styles.registerButton}>
-          <Text style={styles.registerButtonText}>Ainda não possui cadastro?</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+                        <Button
+                            title="Login"
+                            type="solid"
+                            raised={true}
+                            onPress={handleSubmit}
+                            buttonStyle={styles.button}
+                            titleStyle={styles.buttonTitle}
+                            containerStyle={styles.buttonContainer}
+                        />
+
+                        <Button
+                            title="Ainda não possui cadastro?"
+                            type="clear"
+                            onPress={() => {
+                                navigation.navigate("Cadastro");
+                            }}
+                            titleStyle={styles.registerButtonText}
+                        />
+                    </View>
+                )}
+            </Formik>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF",
+    },
 
-  logo: {
-    width: 180,
-    height: 150,
-    resizeMode: 'contain',
-  },
+    logo: {
+        width: 180,
+        height: 150,
+        resizeMode: "contain",
+    },
 
-  form: {
-    alignSelf: 'stretch',
-    marginTop: 30,
-    paddingHorizontal: 28
-  },
+    form: {
+        alignSelf: "stretch",
+        marginTop: 30,
+        paddingHorizontal: 28,
+    },
 
-  label: {
-    fontSize: 20,
-    marginBottom: 10,
-    fontWeight: 'bold',
-  },
+    inputLabel: {
+        fontSize: 18,
+        color: "#555",
+        marginBottom: 5,
+    },
 
-  input: {
-    borderWidth: 1,
-    borderRadius: 2,
-    borderColor: '#ddd',
-    paddingHorizontal: 15,
-    fontSize: 16,
-    height: 40,
-    marginBottom: 16
-  },
+    input: {
+        color: "#444",
+    },
 
-  button: {
-    marginTop: 10,
-    borderRadius: 2,
-    borderWidth: 2,
-    borderColor: '#000',
-    height: 45,
-    backgroundColor: '#6afea8',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  buttonText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-
-  registerButton: {
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  registerButtonText: {
-    fontSize: 16,
-    color: '#45a16b',
-  }
+    button: {
+        backgroundColor: "#2352FF",
+    },
+    buttonTitle: {
+        fontSize: 18,
+    },
+    buttonContainer: {
+        margin: 10,
+    },
+    registerButtonText: {
+        fontSize: 16,
+        color: "#45a16b",
+    },
+    error: {
+        fontSize: 13,
+        fontStyle: "italic",
+    },
 });
