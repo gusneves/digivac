c65f0c9b114a9ce1f1e6bceb90f36ea652b7e088
Lilith <lilith@pop-os.localdomain>
2020-09-28 08:59:48 -0300

    feito o front-end da home

--- src/methods/loadVacs.js
+++
@@ -1,5 +0,0 @@
-//mover td q for relacionado a vacina aq
-
-//load todas
-
-//load uma
--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000'
-});
-api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-
-export default api;
