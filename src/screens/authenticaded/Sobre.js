c0efcfd5e00b58a5bf36325107d61df5dba75a42
Lilith <lilith@pop-os.localdomain>
2020-10-07 15:25:24 -0300

    Tela de sobre

--- src/screens/authenticaded/Sobre.js
+++ src/screens/authenticaded/Sobre.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-export default class Sobre extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Sobre</Text>
-      </View>
-    );
-  }
+import logo from "../../assets/logo.png";
+
+export default function Sobre() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <Text style={styles.titulo}>Quem somos?</Text>
+      <Text style={styles.texto}>
+        A DigiVac é um projeto desenvolvido pelos alunos de informática do Colégio Técnico Industrial "Prof. Isaac Portal Roldán"
+       como trabalho de conclusão de curso. Os dois principais objetivos de nosso projeto são garantir informações sobre as vacinas, assim como
+       sobre as doenças que elas previnem,
+        e também de fornecer uma gestão de dados da carteira nacional de vacinação de maneira totalmente digital, prevenindo contra a perda de informações 
+        caso ocorra extravio do documento físico.
+      </Text>
+      <Text style={styles.titulo}>Desenvolvedores</Text>
+      <Text style={styles.texto}>
+        Daniel Pires, Eduardo Figueiredo, Giovani Bello, Gustavo Neves,
+       Laura Rocha, Maria Luiza Alves, Matheus Ranzani, Paula Dobkowski e Pedro Moço
+      </Text>
+    </View>
+  );
+  
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +30,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#FFF"
+    backgroundColor: "#FFF",
+    padding: 20,
+    paddingTop: 50
   },
+
+  logo: {
+    width: 150,
+    height: 125,
+    resizeMode: "contain",
+    marginBottom: 20
+  },
+  titulo: {
+    fontStyle: "italic",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginBottom:10,
+    fontSize: 18,
+  },
+  texto: {
+    fontSize: 16,
+    textAlign: "justify",
+    marginBottom: 10
+  }
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.112:3000'
+  baseURL: 'http://10.10.10.10:3000'
 });
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
