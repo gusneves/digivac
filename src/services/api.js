154d19c8a197222e2bf5d616e40d1eb043144f3b
uglyd0cky <eaterofworms@outlook.com>
2020-10-22 03:26:56 -0300

    api heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.21:3000'
+    baseURL: 'https://api-db-digivac.herokuapp.com'
 });
 
 api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
